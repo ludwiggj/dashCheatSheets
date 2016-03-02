f3f1c418d9e8a6aa16e779c03a93f7ad53e17baf
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2016-03-02 15:19:50 +0000

    Split vim cheatsheet into MacVim and Nerd

---
+++ mvim.rb
@@ -0,0 +1,49 @@
+# MacVim cheat sheet: see https://github.com/Kapeli/cheatsheets for info
+cheatsheet do
+  title 'MacVim'  
+  docset_file_name 'MacVim'
+  keyword 'mvimc'
+
+  introduction 'My *awesome* MacVim cheat sheet'
+
+  category do
+    id 'MacVim'
+
+    entry do
+      command 'bd'
+      name 'Buffer delete'
+      notes 'Closes current file, and Vim if last file'
+    end
+
+    entry do
+      command 'V'
+      name 'Visual mode'
+      notes 'Select block, line by line (via V or CR)'
+    end
+
+    entry do
+      command ',cc'
+      name 'Comment line/block'
+    end
+    
+    entry do
+      command ',cu'
+      name 'Uncomment line/block'
+    end
+    
+    entry do
+      command ',m'
+      name 'Toggle NERD tree window'
+    end
+    
+    entry do
+      command ',.'
+      name 'Recently opened files'
+    end
+    
+    entry do
+      command 'u'
+      name 'Undo'
+    end
+  end 
+end
---
+++ nerd.rb
@@ -0,0 +1,33 @@
+# Nerdtree cheat sheet: see https://github.com/Kapeli/cheatsheets for info
+cheatsheet do
+  title 'Nerdtree'  
+  docset_file_name 'Nerdtree'
+  keyword 'nerdc'
+
+  introduction 'My *awesome* nerdtree cheat sheet'
+
+  category do
+    id 'nerdtree'
+
+    entry do
+      command 'I'
+      name 'Toggle hidden files'
+    end
+
+    entry do
+      command 'm'
+      name 'Open NERD tree menu'
+      notes 'Used to add/delete files etc.'
+    end
+    
+    entry do
+      command 'R'
+      name 'Refresh directory list'
+    end
+    
+    entry do
+      command 's'
+      name 'Open file in split window'
+    end
+  end
+end
